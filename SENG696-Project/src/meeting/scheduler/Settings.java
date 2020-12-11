67bd69bc94a92ec3544e588a9eaa5d340595cdaf
Jayadev1127 <jayadev1127@gmail.com>
2020-12-11 16:55:33 -0700

    comment

--- SENG696-Project/src/meeting/scheduler/Settings.java
+++ SENG696-Project/src/meeting/scheduler/Settings.java
@@ -1,8 +1,6 @@
 package meeting.scheduler;
 
-/**
- * Created by user on 16-01-2016.
- */
+
 public class Settings {
     public static final boolean debug = true;
     public static final int numDays = 5;
